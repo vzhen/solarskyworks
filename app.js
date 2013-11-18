52ec78fe462b27ce5453dfd572eafb6bda0ea576
vzhen <wcchok@gmail.com>
2013-11-18 12:38:50 +0800

    Added Demo Data

---
+++ app.js
@@ -0,0 +1,60 @@
+var dependencies = [
+					'ngSanitize',
+					'ngRoute',
+					];
+
+var app = angular.module('app', dependencies)
+	.config([	'$routeProvider',
+		function($routeProvider) {
+			$routeProvider.when('/', {
+				templateUrl: 'partials/home.html'
+			});
+			$routeProvider.when('/services', {
+				templateUrl: 'partials/services.html',
+				controller: 'servicesCtrl'
+			});
+			$routeProvider.when('/projects', {
+				templateUrl: 'partials/projects.html',
+				controller: 'projectsCtrl'
+			});
+			$routeProvider.when('/findus', {
+				templateUrl: 'partials/findus.html'
+			});
+		}
+	])
+
+	.controller('servicesCtrl', [
+		'$scope',
+		'$http',
+		function($scope, $http) {
+			$http({
+				method: 'GET',
+				url: 'data/data.json'
+			}).
+				success(function(data, status, headers, config) {
+			    $scope.services = data.services;
+			  }).
+			  error(function(data, status, headers, config) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			  });
+		}
+	])
+
+	.controller('projectsCtrl', [
+		'$scope',
+		'$http',
+		function($scope, $http) {
+			$http({
+				method: 'GET',
+				url: 'data/data.json'
+			}).
+				success(function(data, status, headers, config) {
+			    $scope.projects = data.projects;
+			  }).
+			  error(function(data, status, headers, config) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			  });
+		}
+	])
