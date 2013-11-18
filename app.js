f3b02b44ab9d897a024c79b4bf9b31cc7ce7369d
vzhen <wcchok@gmail.com>
2013-11-18 16:55:00 +0800

    Fixed orderBy

--- app.js
+++ app.js
@@ -58,3 +58,17 @@ var app = angular.module('app', dependencies)
 			  });
 		}
 	])
+
+	.filter('orderObjectBy', function() {
+	  return function(items, field, reverse) {
+	    var filtered = [];
+	    angular.forEach(items, function(item) {
+	      filtered.push(item);
+	    });
+	    filtered.sort(function (a, b) {
+	      return (a[field] > b[field]);
+	    });
+	    if(reverse) filtered.reverse();
+	    return filtered;
+	  };
+	})
